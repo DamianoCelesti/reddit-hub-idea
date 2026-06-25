5d13a2a184b1b710a93fee9e547929c99732620b
Damiano Celesti <Damio9422@hotmail.it>
2026-06-25 22:52:42 +0200

    add react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,21 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import SavedPosts from "./pages/SavedPosts";
 
 function App() {
-    return <Home />;
+    return (
+        <BrowserRouter>
+            <nav className="navbar">
+                <Link to="/">Home</Link>
+                <Link to="/saved">Post salvati</Link>
+            </nav>
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/saved" element={<SavedPosts />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
